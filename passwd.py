6d877045f0a74750818663bd71afe09ca3ec12ad
Lien Ｗ.N <Lien@Liende-MacBook-Pro.local>
2022-06-03 01:25:27 +0800

    passwdUpdate

--- passwd.py
+++ passwd.py
@@ -1,11 +1,13 @@
 i = 3 #剩餘機會
-while True:
+while i > 0:
+	i = i - 1
 	passwd = input('請輸入密碼:')
 	if passwd == 'a123456':
 		print('登入成功!')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤!還有', i,'次機會')
-		if i == 0:
-			break
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i,'次機會')
+		else:
+			print('不要亂登入哦!')

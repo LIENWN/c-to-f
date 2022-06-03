75d86714e513a332dc0bd89bf6709633f4e0a6bf
Lien Ｗ.N <Lien@Liende-MacBook-Pro.local>
2022-06-03 20:24:24 +0800

    Update_randomguess

--- ram.py
+++ ram.py
@@ -1,14 +1,22 @@
 
 import random
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數赤範圍結束值:')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('請猜一個數字:')
 	num = int(num)
 	if num == r:
 		print('終於答對了!')
+		print('這是你猜的第', count,'次!')
 		break
 	elif num > r:
 		print('比答案大!')
 	elif num < r:
 		print('比答案小!')
+	print('這是你猜的第', count,'次!')

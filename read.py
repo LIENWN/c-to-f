934a8447f05d5eaf389890d9ef629c0eb427d6d0
Lien Ｗ.N <Lien@Liende-MacBook-Pro.local>
2022-06-04 00:46:02 +0800

    caculate avg length of read.py

--- read.py
+++ read.py
@@ -6,7 +6,10 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0: #除以1000餘數為0(%用來求餘數)
         	print(len(data))
-print(len(data))
-print(data[0])
-print('---------------------------')
-print(data[1])
+print('檔案已讀取完畢, 總共有', len(data),'筆資料!')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('每筆留言的平均長度為', sum_len / len(data))
